e320b08ed53b5545af9f6b9bf90aa8bda0255eff
Chinesejunzai <Chinesejunzai@163.com>
2017-07-06 12:12:36 +0800

    回退

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -1,12 +1,20 @@
+#coding=utf-8
 from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
+
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
+    umail = models.CharField(max_length=20)
+    ushou = models.CharField(default='', max_length=10)
+    uaddress = models.CharField(default='', max_length=100)
+    ucode = models.CharField(default='', max_length=6)
+    uphone = models.CharField(default='', max_length=11)
 
-    def __str__(self):
-        return "%s" % self.uname
+
+
+
 
 
 
